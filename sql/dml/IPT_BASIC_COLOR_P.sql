030a3ff1099c736a6eee1c9093e21f3c762e2b46
Ben Smith <benjamin.smith@fdbhealth.com>
2022-01-19 20:08:20 -0500

    change dml sql to single insert statement

--- sql/dml/IPT_BASIC_COLOR_P.sql
+++ sql/dml/IPT_BASIC_COLOR_P.sql
@@ -1,76 +1,20 @@
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (1, 'black', 'black', 'blck', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (2, 'blue', 'blue', 'blue', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (3, 'brown', 'brown', 'brwn', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (4, 'clear', 'clear', 'clr', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (5, 'gold', 'gold', 'gold', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (6, 'gray', 'gray', 'gray', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (7, 'green', 'green', 'grn', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (8, 'lavender', 'lavndr', 'lvdr', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (9, 'multi-colored', 'mlt-clr', 'mult', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (10, 'off-white', 'off-wht', 'owht', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (11, 'orange', 'orange', 'orng', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (12, 'peach', 'peach', 'pch', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (13, 'pink', 'pink', 'pink', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (14, 'purple', 'purple', 'prpl', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (15, 'red', 'red', 'red', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (16, 'tan', 'tan', 'tan', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (17, 'turquoise', 'turquos', 'turq', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (18, 'white', 'white', 'wht', '0');
-Insert into IPT_BASIC_COLOR_P
-   (ID, DESCRIPTION, ABBREVIATION, SHORT_ABBREVIATION, DO_NOT_USE_IND)
- Values
-   (19, 'yellow', 'yellow', 'yell', '0');
+INSERT INTO public.ipt_basic_color_p (id,description,abbreviation,short_abbreviation,do_not_use_ind) VALUES
+	 (1,'black','black','blck','0'),
+	 (2,'blue','blue','blue','0'),
+	 (3,'brown','brown','brwn','0'),
+	 (4,'clear','clear','clr','0'),
+	 (5,'gold','gold','gold','0'),
+	 (6,'gray','gray','gray','0'),
+	 (7,'green','green','grn','0'),
+	 (8,'lavender','lavndr','lvdr','0'),
+	 (9,'multi-colored','mlt-clr','mult','0'),
+	 (10,'off-white','off-wht','owht','0'),
+	 (11,'orange','orange','orng','0'),
+	 (12,'peach','peach','pch','0'),
+	 (13,'pink','pink','pink','0'),
+	 (14,'purple','purple','prpl','0'),
+	 (15,'red','red','red','0'),
+	 (16,'tan','tan','tan','0'),
+	 (17,'turquoise','turquos','turq','0'),
+	 (18,'white','white','wht','0'),
+	 (19,'yellow','yellow','yell','0');
